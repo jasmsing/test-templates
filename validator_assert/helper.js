e120e07cf0f341ca960eec73d75216c8c6f7f44c
Mark Macdonald <mamacdon@ca.ibm.com>
2016-11-24 15:27:09 -0500

    Fix up validator_assert problems

--- validator_assert/helper.js
+++ validator_assert/helper.js
@@ -18,65 +18,78 @@ function respond(event, msg, response) {
 }
 
 function respondErr(event, msg) {
-	respond(event, "respond with error", {
+	var lastError = {
 		status: "error",
 		statusCode: 500,
 		description: msg
-	})
+	};
+	respond(event, "respond with error", lastError);
 }
 
-var barChecked = false;
+var populateCount = 0;
+var lastError;
 
 function receiveMessage(event) {
 	if (!event.data) {
 		return
 	}
 	consoleLog("received: " + JSON.stringify(event.data));
+
+	// If we fail at any point, stay failing
+	if (lastError) {
+		respond(event, "respond", lastError);
+		return;
+	}
+
 	if (event.data.request === 'authenticate') {
 		respond(event, "respond auth ", {
 			statusCode: 200
 		});
 	} else if (event.data.request === 'populate') {
 		var props = event.data.properties;
+		var fail = false;
 
-		// check for problems
-		if (errChangedMissing(event, props)) {
-			return;
+		populateCount++;
+		if (populateCount === 1) {
+			// Initial populate, every field has its default value
+			fail = fail || assertValues(event, props, { foo: "", bar: "honk" });
+		} else if (populateCount === 2) {
+			// Populate after edit, values are set by the 'user'
+			fail = fail || assertValues(event, props, { foo: "foo2", bar: "honk2" });
+			fail = fail || assertChanged(event, props.foo);
 		}
-		if (errValuesBad(event, props)) {
+		if (fail) {
 			return;
 		}
 
 		// otherwise respond ok
 		respond(event, "respond populate", event.data.properties);
 	} else if (event.data.request === 'validate') {
-		respond(event, "respond validate ", {
-			description: "This is an error returned from validate"
-		});
+		respond(event, "respond validate ", {});
 	}
 }
 
 // Check that `changed == true` was provided when a value was modified
-function errChangedMissing(event, props) {
-	var bar = props.bar;
-	// Hack: we know bar's initial value was "honk". The first time it changes from "honk",
-	// verify that the `changed` flag was sent
-	if (bar && bar.value !== "honk") {
-		if (!barChecked && !bar.changed) {
-			respondErr(event, "Expected bar.changed == true but found " + JSON.stringify(props.bar))
-			return true; //handled
-		}
-		barChecked = true;
+// returns false if fialed
+function assertChanged(event, obj) {
+	if (!obj.changed) {
+		respondErr(event, "Expected a `changed == true` field in " + JSON.stringify(obj))
+		return true;
 	}
 	return false;
 }
 
-function errValuesBad(event, props) {
-	if (props.foo.value !== "foo0") {
-		respondErr("Expected foo.value == 'foo0', found: " + JSON.stringify(props.foo));
-		return true;
-	}
-	return false;
+// returns false if failed
+function assertValues(event, props, expected) {
+	return Object.keys(expected).some(function(prop) {
+		var want = expected[prop];
+		var got = props[prop].value;
+		if (want !== got) {
+			respondErr(event, "Wanted " + prop + ".value == '" + want + "', got " + JSON.stringify(props[prop]))
+			return true;
+		}
+		return false;
+	});
 }
 
 window.addEventListener("message", receiveMessage, false);
