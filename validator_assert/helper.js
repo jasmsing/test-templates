2736fa42213827997daa45285ed9feb220921e5a
Mark Macdonald <mamacdon@ca.ibm.com>
2016-11-24 13:35:31 -0500

    Assert more things

--- validator_assert/helper.js
+++ validator_assert/helper.js
@@ -17,6 +17,14 @@ function respond(event, msg, response) {
 	return response;
 }
 
+function respondErr(event, msg) {
+	respond(event, "respond with error", {
+		status: "error",
+		statusCode: 500,
+		description: msg
+	})
+}
+
 var barChecked = false;
 
 function receiveMessage(event) {
@@ -25,39 +33,50 @@ function receiveMessage(event) {
 	}
 	consoleLog("received: " + JSON.stringify(event.data));
 	if (event.data.request === 'authenticate') {
-		respond(event,"respond auth ", {
+		respond(event, "respond auth ", {
 			statusCode: 200
 		});
 	} else if (event.data.request === 'populate') {
-		// var rc = {
-		// 	request: event.data.request,
-		// 	response: event.data.properties
-		// };
-		// event.source.postMessage(rc, "*");
-
 		var props = event.data.properties;
-		var bar = props.bar;
-		// Hack: we know bar's initial value was "honk". The first time it changes from "honk",
-		// verify that the `changed` flag was sent
-		if (bar && bar.value !== "honk") {
-			if (!barChecked && !bar.changed) {
-				respond(event, "respond populate", {
-					status: "error",
-					statusCode: 500,
-					description: "Expected bar.changed == true but found " + JSON.stringify(props.bar)
-				});
-			}
-			barChecked = true;
+
+		// check for problems
+		if (errChangedMissing(event, props)) {
+			return;
+		}
+		if (errValuesBad(event, props)) {
 			return;
 		}
 
-		// respond ok
+		// otherwise respond ok
 		respond(event, "respond populate", event.data.properties);
 	} else if (event.data.request === 'validate') {
 		respond(event, "respond validate ", {
 			description: "This is an error returned from validate"
 		});
 	}
+}
+
+// Check that `changed == true` was provided when a value was modified
+function errChangedMissing(event, props) {
+	var bar = props.bar;
+	// Hack: we know bar's initial value was "honk". The first time it changes from "honk",
+	// verify that the `changed` flag was sent
+	if (bar && bar.value !== "honk") {
+		if (!barChecked && !bar.changed) {
+			respondErr(event, "Expected bar.changed == true but found " + JSON.stringify(props.bar))
+			return true; //handled
+		}
+		barChecked = true;
+	}
+	return false;
+}
+
+function errValuesBad(event, props) {
+	if (props.foo.value !== "foo0") {
+		respondErr("Expected foo.value == 'foo0', found: " + JSON.stringify(props.foo));
+		return true;
+	}
+	return false;
 }
 
 window.addEventListener("message", receiveMessage, false);
